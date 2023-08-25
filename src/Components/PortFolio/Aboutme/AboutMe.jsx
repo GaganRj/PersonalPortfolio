b104ff2460fda6249933f8d0eeb1708120e48c03
GaganRj <141042388+GaganRj@users.noreply.github.com>
2023-08-25 12:00:15 +0530

    Update AboutMe.jsx

--- src/Components/PortFolio/Aboutme/AboutMe.jsx
+++ src/Components/PortFolio/Aboutme/AboutMe.jsx
@@ -35,6 +35,7 @@ const AboutMe = () => {
 
                     })
                 }
+                 <br/>
                 <div className={Style.skills}>
                     {
                         SkillHead.map((skilllist) => {
