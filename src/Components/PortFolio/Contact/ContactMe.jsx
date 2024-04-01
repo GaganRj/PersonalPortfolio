8b7ff58552c152649a84c43d0e56cdd61b504f8e
Gaganraj <141042388+GaganRj@users.noreply.github.com>
2024-04-01 12:29:03 +0530

    Update ContactMe.jsx
    cv

--- src/Components/PortFolio/Contact/ContactMe.jsx
+++ src/Components/PortFolio/Contact/ContactMe.jsx
@@ -28,7 +28,7 @@ const ContactMe = () => {
                     <PhoneOutlinedIcon style={{ color: "#FF007A" }} />
                     <a href="tel:+919148525489"><span style={{ color: "white" }}>+91 9148525489</span></a>
                 </div>
-                <a target="blank" href='https://drive.google.com/file/d/1dlFbQt7W78O7yyYFmbh5qnJWWtU74AbO/view?usp=drive_link' >
+                <a target="blank" href='https://drive.google.com/file/d/15bt9ZzGA04vkOsjYgupty_sG7kAg-Qoy/view?usp=sharing'>
                     <div className="box">
                         <div className="bt btn-three">
                             <span style={{ color: "white", fontWeight: "600" }}>Download CV</span>
